abfe185c0eae4cb082826751f6aff13b8f3021aa
dealeska <zenya.com44@gmail.com>
2021-09-28 18:02:46 +0300

    1st task of the 2nd lab done

---
+++ MPP_Lab_2/Mutex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MPP_Lab_2
+{
+    internal class Mutex
+    {
+        private const int unlockedThreadID = 0;
+        public int lockedThreadID = 0;
+        private int currentThreadID = Thread.CurrentThread.ManagedThreadId;    
+
+        public void Lock()
+        {                      
+            SpinWait.SpinUntil(() => Interlocked.CompareExchange(ref lockedThreadID, currentThreadID, unlockedThreadID) == currentThreadID );
+            Console.WriteLine("Locked");
+        }
+
+        public void Unlock()
+        {
+            if (Interlocked.CompareExchange(ref lockedThreadID, unlockedThreadID, currentThreadID) != currentThreadID)
+            {
+                return;
+            }
+            Console.WriteLine("Unlocked");
+        }
+    }
+}
---
+++ MPP_Lab_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MPP_Lab_2
+{
+    internal class Program
+    {
+        public static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread thread = new Thread(getThreadInfo);
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void getThreadInfo()
+        {
+            mutex.Lock();
+            Console.WriteLine($"I {Thread.CurrentThread.ManagedThreadId} thread!");
+            mutex.Unlock();
+            Thread.Sleep(250);
+        }
+    }
+}
