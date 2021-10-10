7ffb410a4e05f2a5424a9c360cb954d4f5630dbd
dealeska <zenya.com44@gmail.com>
2021-10-10 20:19:04 +0300

    add second task

---
+++ MPP_Lab_2/OSHandle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MPP_Lab_2
+{
+    class OSHandle : IDisposable
+    {
+        public int Handle { get; private set; }
+        private Mutex mutex = new Mutex();
+        private bool disposed = false;
+
+
+        public OSHandle(int handle)
+        {
+            Handle = handle;
+        }
+
+        ~OSHandle()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+                disposed = true;                
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            mutex.Lock();
+
+            if (!disposed)
+            {
+                if (Handle != 0)
+                {
+                    Handle = 0;
+                }
+                disposed = true;
+            }
+
+            mutex.Unlock();
+        }        
+    }
+}
