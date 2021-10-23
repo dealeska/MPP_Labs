d6eb46e097ab5388f4b5e7aa44560025024e22be
dealeska <zenya.com44@gmail.com>
2021-10-23 17:57:16 +0300

    Added 1st task

---
+++ MPP_Lab_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<string> met = new List<string>();
+            string path;
+            Console.WriteLine("Path to dll or exe: ");
+            path = Console.ReadLine();
+
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] type = asm.GetTypes();
+
+            foreach (Type t in type)
+            {
+                Console.WriteLine($"Class Name: {t.Name}\nNamespace: {t.Namespace}\nPublic methods:");
+                MethodInfo[] method = t.GetMethods();
+                foreach (var m in method)
+                {
+                    if (m.IsPublic)
+                    {
+                        Console.WriteLine(m.Name);
+                        met.Add(m.Name);
+                    }
+                }                
+                Console.WriteLine();
+            }
+
+            var sortedMethods = met.OrderBy(m => m);
+
+            foreach (var m in sortedMethods)
+            {                
+                Console.WriteLine(m);                                  
+            }
+            Console.ReadLine();
+        }
+    }
+}
