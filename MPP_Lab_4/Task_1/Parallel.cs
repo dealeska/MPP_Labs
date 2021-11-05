e2cd2bd08196a5fda7b9e6497d2682438171abf3
dealeska <zenya.com44@gmail.com>
2021-11-05 14:24:19 +0300

    First task done

---
+++ MPP_Lab_4/Task_1/Parallel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_1
+{
+    class Parallel
+    {
+        public delegate void TaskDelegate();
+
+        public static void WaitAll(TaskDelegate[] delegates)
+        {           
+            foreach (var del in delegates)
+            {                
+                ThreadPool.QueueUserWorkItem(ThreadProc, del);
+            }            
+        }
+
+        private static void ThreadProc(Object stateInfo)
+        {
+            if (stateInfo is TaskDelegate)
+            {
+                ((TaskDelegate)stateInfo).Invoke();
+            }
+        }
+    }
+}
---
+++ MPP_Lab_4/Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.TaskDelegate[] delegates = new Parallel.TaskDelegate[] { 
+                DoWork, DoWork, DoWork, DoWork, DoWork, DoWork,
+                DoWork, DoWork, DoWork, DoWork, DoWork, DoWork,
+                DoWork, DoWork, DoWork, DoWork, DoWork, DoWork
+            };
+
+            Parallel.WaitAll(delegates);
+            Thread.Sleep(2000);
+            Console.ReadLine();
+        }
+
+        public static void DoWork()
+        {            
+            Console.WriteLine("Поток с номером {0}.", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+        }
+    }
+}
